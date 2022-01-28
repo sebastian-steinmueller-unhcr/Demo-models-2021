ef0d4f4202e79b519281b75da0e6c663de3f2d48
Sebastian Steinmueller <steinmul@unhcr.org>
2022-01-28 19:03:59 +0000

    first model (simple binomial for children)

--- demomodels_test_afg.R
+++ demomodels_test_afg.R
@@ -31,17 +31,33 @@ demref2020.ori.asy.age$agesex <- with(demref2020.ori.asy.age, cbind(female_0_4, 
                                             male_0_4, male_5_11, male_12_17,male_18_59, male_60)) # needed for binomial models
 
 demref2020.ori.asy.age <- demref2020.ori.asy.age %>% filter(origin_iso3 == "AFG") %>%
-  select(origin, origin_iso3, origin_country, asylum, asylum_iso3, asylum_country,
+  select(origin, origin_iso3, origin_country, asylum, asylum_iso3, c,
          children, totalEndYear, typeOfDisaggregationAge)
 
 View(demref2020.ori.asy.age)
+
+t.disaggregation <- demref2020.ori.asy.age %>% 
+  group_by(origin_country, asylum_country, typeOfDisaggregationAge) %>%
+  summarise(totalEndYear = sum(totalEndYear),
+            children = sum(children)) %>%
+  pivot_wider(id_cols = c(origin_country, asylum_country),
+              names_from = typeOfDisaggregationAge,
+              values_from = totalEndYear)
+  
+
+
 
 ####### II. Simple models without covariates #######
 
 
 #### II.A Binomial models to estimate proportion of children and women only ##### 
 
+### children only
 
+m.children.1 <- brm(data = demref2020.ori.asy.age %>% filter(typeOfDisaggregationAge  == "Age"), family = binomial,
+      children | trials(totalEndYear) ~ 1 + (1|asylum_iso3),
+      prior(normal(0, 10), class = Intercept),
+      seed = 10)
 
 
 
