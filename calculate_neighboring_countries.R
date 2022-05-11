ee6776a90764fbd1f8f93a7e286a20ac04a7e3f1
Yukun Pei <13552787009@163.com>
2022-05-11 04:34:09 -0400

    distance matrix
    added distance matrix as distance.Rdata from CEPII geo dataset

--- calculate_neighboring_countries.R
+++ calculate_neighboring_countries.R
@@ -1,3 +1,4 @@
+##### Helper script calculating neighboring country matrix and distance matrix #####
 library(dplyr)
 library(tidyr)
 library(rgeos)
@@ -9,7 +10,7 @@ library("ggplot2")
 library("rgdal")
 library("scales")
 
-
+####################### Calculate neighboring country from UN map files ####################
 # Read in UN shapefiles
 # Read in the UN cartography polygon shapefile (no antarctica)
 path.basic <- "."
@@ -66,105 +67,18 @@ neighbor.summary <- all.neighbors %>%
   arrange(desc(num))
 neighbor.summary
 
+save(neighbor.summary, all.neighbors, file = 'data/neighbor.Rdata')
 
-#####calculate distance
-# Read in UN shapefiles 
-# Read in the UN cartography polygon shapefile (no antarctica)
-world.un <- readOGR(map.dir, "un-world-2012-no-antartica-10pct") # (requires rgdal package)
-# convert to Robinson projection (the projection preferred by UN Cartography)
-proj4string(world.un) <- CRS("+proj=longlat +ellps=WGS84") # (requires sp package)
-world.robin <- spTransform(world.un, CRS("+proj=robin")) # (requires rgdal package)
+####################### Calculate distance matrix from CEPII geo dataset http://www.cepii.fr/CEPII/en/bdd_modele/download.asp?id=8 ####################
 
-# Read in the Un Cartography shapefile with country/area boundaries
-bnd.un <- readOGR(map.dir, "2012_UNGIWG_bnd_ln_01")
-# convert to Robinson projection
-proj4string(bnd.un) <- CRS("+proj=longlat +ellps=WGS84")
-bnd <- spTransform(bnd.un, CRS("+proj=robin"))
+geo_data <- read_csv('data/Gravity_V202102.csv') %>% filter(year == 2019)
 
-# Read in the Un Cartography shapefile with coastlines
-cst.un <- readOGR(map.dir, "2012_UNGIWG_cst_ln_01")
-# convert to Robinson projection
-proj4string(cst.un) <- CRS("+proj=longlat +ellps=WGS84")
-cst <- spTransform(cst.un, CRS("+proj=robin"))
-# remove Antarctica -- this is a bit clunky, but it works
-cst.df <- fortify(cst)
-cst.df <- cst.df[cst.df$lat>=-6285430,]
+distance_df_long <- geo_data %>% select(orig_iso3 = iso3_o, dest_iso3 = iso3_d,orig_iso3_id = iso3num_o,dest_iso3_id = iso3num_d, distance = dist ) %>% mutate(distance = ifelse(dest_iso3 == orig_iso3, 0, distance)) 
+distance_df_wide <- distance_df_long %>%  select(-dest_iso3_id, -orig_iso3_id) %>% pivot_wider(names_from = orig_iso3, values_from = distance) 
+distance_matrix <- data.matrix(distance_df_wide[,-1])
+rownames(distance_matrix) <-distance_df_wide %>% pull(1)
 
-# Read in the Un Cartography shapefile with lakes
-lks.un <- readOGR(map.dir, "2012_UNGIWG_lks_ply_01")
-# convert to Robinson projection
-proj4string(lks.un) <- CRS("+proj=longlat +ellps=WGS84")
-lks <- spTransform(lks.un, CRS("+proj=robin"))
-lks.df <- fortify(lks)
-
-# Read in the Un Cartography shapefile with Antarctica
-wld.un <- readOGR(map.dir, "un-world-2012-65pct")
-ant.un <- wld.un[wld.un$TERR_NAME=="Antarctica",]
-rm(wld.un)
-# convert to Robinson projection
-proj4string(ant.un) <- CRS("+proj=longlat +ellps=WGS84")
-ant <- spTransform(ant.un, CRS("+proj=robin"))
-ant.df <- fortify(ant)
-ant.df$color.code <- NA
-if (plot.coastlines==TRUE){ ant.df$color.code <- boundary.color }
-
-world.robin@data$id <- as.integer(rownames(world.robin@data))+1
-
-world.robin.df <- data.frame()
-for(i in 1:length(world.robin)){
-  new.df <- fortify(world.robin[world.robin$id == i,]) %>% mutate(id = i)
-  world.robin.df <- rbind(world.robin.df, new.df)
-}
-world.robin.df <- left_join(world.robin.df, select(world.robin@data,id,ISO3_CODE),by = 'id')
-
-bnd@data$id <- as.integer(rownames(bnd@data))+1
-
-bnd.df <- data.frame()
-for(i in 1:length(bnd)){
-  new.df <- fortify(bnd[bnd$id == i,]) %>% mutate(id = i)
-  bnd.df <- rbind(bnd.df, new.df)
-}
-bnd.df <- left_join(bnd.df, select(bnd@data,id,CARTOGRAPH),by = 'id') %>% 
-  mutate(CARTOGRAPH = recode(CARTOGRAPH, 'International boundary line' = 'line', 
-                             "Dashed boundary line" = 'dashed', "Undetermined international dashed boundary line" = 'dashed',
-                             "Dotted boundary line" = 'dotted',"Dotted boundary line (Abyei)" ='dotted', .default = NA_character_)) %>%
-  filter(!is.na(CARTOGRAPH ))
-
-
-
-library(scatterpie)
-#prepare data for pie charts
-places <- data.frame(matrix(ncol = 5, nrow = length(world.un)))
-colnames(places) <- c("ISO3", "TERR_NAME", "long", "lat", "STATUS")
-
-places$ISO3 <- as.character(world.un$ISO3_CODE)
-places$TERR_NAME <- world.un$TERR_NAME
-places$long <- coordinates(world.un)[, 1]
-places$lat <- coordinates(world.un)[, 2]
-places$STATUS <- world.un$STATUS
-places$ISO3[places$TERR_NAME == 'Abyei'] <- 'AB9'
-
-distdf <- places %>% filter(ISO3 != '<NA>', ISO3 %in% union(demref2020 %>% distinct(asylum_iso3) %>% pull(), demref2020 %>% distinct(origin_iso3) %>% pull())) %>%
-  filter(STATUS != 'PT Territory', !(TERR_NAME %in% c('Guernsey','Senkaku Islands','Gaza Strip','Kuril islands')) ) %>%
-  
-  group_by(ISO3) %>%
-  sample_n(1)    
-
-
-distmatrix <- distdf %>% ungroup() %>% select(long,lat) %>% as.matrix()
-
-
-distance_matrix <- distm(distmatrix, fun = distHaversine) %>% as.data.frame()
-rownames(distance_matrix) <- distdf %>% pull(ISO3)
-colnames(distance_matrix) <- distdf %>% pull(ISO3)
-
-m <- as.matrix(distance_matrix)
-distance_long <- data.frame(col=colnames(m)[col(m)], row=rownames(m)[row(m)], dist=c(m))
-
-
-
-
-save(distance_matrix,distance_long,file = 'data/distance.Rdata')
+save(distance_matrix,distance_df_long,file = 'data/distance.Rdata')
 
 
 
